6d9d466976610214d391ca17d636c6a9a30745f2
Marcellous Curtis <1mcj001@gmail.com>
2022-02-13 13:19:42 -0600

    finished

---
+++ app.js
@@ -0,0 +1,19 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+
+const PI = 3.14159265359
+
+// quiz 
+// 1. what is the differnce between vat and let 
+//  - You can redeclare var but you cant redeclare let
+
+// 2. what is the difference between var and const?
+//  - you cant reassign nor redeclare const, where as you can do both with var
+
+// 3. what is the difference between let and const?
+//  - you can reassign let but you cant reassign const
+
+// 4. what is hoisting 
+//  - Hoisting is associated with var the declarations are read first then its values are added later but nearly instantly. but here are run at different points. 
